9e1bce7f64fa5bf2dcfb9e002507769cb2af4e2e
DIS686 <106627508+maksimd31@users.noreply.github.com>
2023-07-10 11:56:39 +0300

    one 1.2

--- main.py
+++ main.py
@@ -4,18 +4,25 @@ from id_bot import id_bot as api
 
 bot = Bot(api.bot)
 dp = Dispatcher(bot)
+sti = 'CAACAgIAAxkBAAEGNZdjWXnu4KTX3oGTRnt6mxm5b4HbQQACIRQAAiHvyUvdPulOAgvvMSoE'
 
-executor.start_polling(dp)
 
-
-@dp.message_handlers(commands=['start'])
+@dp.message_handler(commands=['start'])
 async def start(massage: types.Message):
     markup = types.ReplyKeyboardMarkup()
-    markup.add(types.KeyboardButtonPollType('Открыть веб страницу', web_app=WebAppInfo(
-        url='https://github.com/maksimd31/tg_bot/blob/master/index.html')))
-    await massage.answer('Привет', reply_markup=markup)
+    markup.add(types.KeyboardButton('Открыть веб страницу', web_app=WebAppInfo(
+        url='https://maksimd31.github.io/tg_bot/')))
+    await massage.answer('Привет <b>Dungeon Master</b>', reply_markup=markup,parse_mode='HTML')
+    await bot.send_sticker(massage.from_user.id, sti)
 
 
-# Press the green button in the gutter to run the script.
+@dp.message_handler(commands=['give'])
+async def start(massage: types.Message):
+    await bot.send_sticker(massage.from_user.id, sti)
+
+
+# # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    pass
+    executor.start_polling(dp)
+
+#     pass
